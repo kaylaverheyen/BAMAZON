07a23148ea851e84b4f42f4418daff46057efd3e
kaylaverheyen <kaylaverheyen@icloud.com>
2018-12-27 20:06:45 -0600

    updated with inquirer and cli-table

--- bamazon.sql
+++ bamazon.sql
@@ -13,6 +13,6 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
-INSERT INTOproducts (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("shirt", "clothing", 25.99, 15), ("denim_jacket", "clothing", 80.00, 8), ("polariod_instax_mini_9_camera", "cameras", 79.00, 8), ("cactus_lamp", "home_goods", 56.00, 4), ("white_picture_frame", "home_goods", 24.99, 7), ("ceramic_flower_pot", "home_goods", 16.99, 10), ("houndstooth_scarf", "clothing", 30.99, 8), ("2019_planner", "office_supplies", 15.99, 9), ("wicker_bag", "accessories", 42.00, 4), ("beaded_blue_necklace", "accessories", 26.00, 6);
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,9 @@ const mysql = require("mysql");
 //installed 
 const inquirer = require("inquirer");
 // Require table npm to create a table in node -- need to install!!
-const table = require("table").table;
+//const table = require("table").table;
+var Table = require('cli-table');
+
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -34,137 +36,88 @@ function displayProducts() {
         if (err) {
             throw err;
         };
-        var data = res.forEach(row => console.log(`id: ${row.id} | ${row.product_name} | $ ${row.price} | how many left: ${row.stock_quantity} | Dept: ${row.department_name}`));
+        // instantiate
+        var table = new Table({
+            head: ['ID', 'Product', 'Department', 'Price', 'Quantity']
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            var productArr = [];
+            for (var key in res[i]) {
+                productArr.push(res[i][key]);
+            }
+            table.push(productArr);
+        }
+
+        console.log(table.toString());
+        //var data = res.forEach(row => console.log(`id: ${row.id} | ${row.product_name} | $ ${row.price} | how many left: ${row.stock_quantity} | Dept: ${row.department_name}`));
         // console.log(data);
+        placeOrder();
     });
 };
 
 
-function makeBid() {
+function placeOrder() {
     inquirer
-        .prompt({
-            name: "postOrBid",
-            type: "rawlist",
-            message: "Would you like to [POST] an auction or [BID] on an auction?",
-            choices: ["POST", "BID"]
-        })
+        .prompt([{
+            name: "ID",
+            type: "input",
+            message: "Which product would you like to buy? Please select an ID"
+        },
+        {
+            name: "Quantity",
+            type: "input",
+            message: "How many would you like to purchase?"
+        }])
         .then(function (answer) {
-            // based on their answer, either call the bid or the post functions
-            if (answer.postOrBid.toUpperCase() === "POST") {
-                postAuction();
-            }
-            else {
-                bidAuction();
-            }
-        });
-}
+            //grab price and id from table 
+            connection.query("SELECT stock_quantity, price FROM products WHERE ?", { id: parseInt(answer.ID) }, function (err, res) {
+                if (err) {
+                    throw err;
+                };
+                if (res[0].stock_quantity >= parseInt(answer.Quantity)) {
+                    var new_quantity = res[0].stock_quantity - parseInt(answer.Quantity);
 
-// function to handle posting new items up for auction
-function postAuction() {
-    // prompt for info about the item being put up for auction
-    inquirer
-        .prompt([
-            {
-                name: "item",
-                type: "input",
-                message: "What is the item you would like to submit?"
-            },
-            {
-                name: "category",
-                type: "input",
-                message: "What category would you like to place your auction in?"
-            },
-            {
-                name: "startingBid",
-                type: "input",
-                message: "What would you like your starting bid to be?",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-        ])
-        .then(function (answer) {
-            // when finished prompting, insert a new item into the db with that info
-            connection.query(
-                "INSERT INTO auctions SET ?",
-                {
-                    item_name: answer.item,
-                    category: answer.category,
-                    starting_bid: answer.startingBid,
-                    highest_bid: answer.startingBid
-                },
-                function (err) {
-                    if (err) throw err;
-                    console.log("Your auction was created successfully!");
-                    // re-prompt the user for if they want to bid or post
-                    start();
-                }
-            );
-        });
-}
 
-function bidAuction() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM auctions", function (err, results) {
-        if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].item_name);
-                        }
-                        return choiceArray;
-                    },
-                    message: "What auction would you like to place a bid in?"
-                },
-                {
-                    name: "bid",
-                    type: "input",
-                    message: "How much would you like to bid?"
-                }
-            ])
-            .then(function (answer) {
-                // get the information of the chosen item
-                var chosenItem;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].item_name === answer.choice) {
-                        chosenItem = results[i];
-                    }
-                }
-
-                // determine if bid was high enough
-                if (chosenItem.highest_bid < parseInt(answer.bid)) {
-                    // bid was high enough, so update db, let the user know, and start over
-                    connection.query(
-                        "UPDATE auctions SET ? WHERE ?",
-                        [
-                            {
-                                highest_bid: answer.bid
-                            },
-                            {
-                                id: chosenItem.id
-                            }
-                        ],
-                        function (error) {
-                            if (error) throw err;
-                            console.log("Bid placed successfully!");
-                            start();
-                        }
-                    );
+                    // update quantity
+                    connection.query("UPDATE products SET ? WHERE ?", [{ stock_quantity: new_quantity }, { id: parseInt(answer.ID) }], function (err, temp) {
+                        if (err) {
+                            throw err;
+                        };
+                        //count price for the order
+                        var p = res[0].price * parseInt(answer.Quantity);
+                        console.log("Order total is: " + p);
+                    });
                 }
                 else {
-                    // bid wasn't high enough, so apologize and start over
-                    console.log("Your bid was too low. Try again...");
-                    start();
+                    console.log("Insufficient Quantity!!");
                 }
+
+                //copy code of display products
+                connection.query("SELECT * FROM products", function (err, res) {
+                    if (err) {
+                        throw err;
+                    };
+                    // instantiate
+                    var table = new Table({
+                        head: ['ID', 'Product', 'Department', 'Price', 'Quantity']
+                    });
+
+                    for (var i = 0; i < res.length; i++) {
+                        var productArr = [];
+                        for (var key in res[i]) {
+                            productArr.push(res[i][key]);
+                        }
+                        table.push(productArr);
+                    }
+
+                    console.log(table.toString());
+                });
+                //end connection 
+
+                connection.end();
             });
-    });
+        });
 }
+
+//}
